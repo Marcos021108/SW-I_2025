fcadc3583cc2b9a3d6ab3ac5e6d06879b5686091
Marcos <161627757+Marcos021108@users.noreply.github.com>
2025-02-13 18:22:08 -0300

    ............................

---
+++ aula02/exemplo01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula02 - SW I</title>
+</head>
+<body>
+    <p>oi anderson, sou o Marcos</p>
+    
+    <?php
+        $nome = "Marcos";
+        echo "<p>oi anderson, sou o Marcos<p>";
+        echo "<p>oi anderson, sou o $nome<p>";
+        echo "<p>oi anderson, sou o". $nome. "<p>";
+    ?>
+    
+</body>
+</html>
